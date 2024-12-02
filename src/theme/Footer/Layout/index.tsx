0fb45fbed6f83266e140eb35915742ab90a0f3a1
Ashish <ashish@ngit.com.np>
2024-12-02 17:28:51 +0545

    Preview Build 3 (#8)
    * Adding Algolia Search (#5)
    
    * feat: initial commit for styles optimization
    
    * feat: header styles
    
    * feat: additional swizzeled components and styles
    
    * feat: add algolia search
    
    * fix: double code
    
    * added: main cicd
    
    * fix: naming
    
    * fix: naming
    
    * fix: console errors
    
    * fix: footer content styles
    
    ---------
    
    Co-authored-by: nexgen-tech <pm@ngit.com.np>

--- src/theme/Footer/Layout/index.tsx
+++ src/theme/Footer/Layout/index.tsx
@@ -14,47 +14,59 @@ export default function FooterLayout({
         "footer--dark": style === "dark",
       })}
     >
-      <div style={{ padding: 0, display:"flex", justifyContent: "center", alignItems: "center" }}>
+      <div
+        style={{
+          padding: 0,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         {(logo || copyright) && (
           <div
             className="footer__bottom text--center"
             style={{ display: "inline-flex", alignItems: "center" }}
           >
-            {logo && <div className="margin-bottom--sm">{logo}</div>}
-            {copyright}
-            {links}
-            <button
-              onClick={() => document.body.scrollIntoView()}
-              style={{
-                display: "flex",
-                alignItems: "center",
-                fontSize: "1rem",
-                background: "none",
-                border: "none",
-                padding: 0,
-                margin: 0,
-                boxShadow: "none",
-                whiteSpace: "nowrap",
-                color: "var( --ifm-font-color-base)",
-              }}
-            >
-              Back to Top{" "}
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+            <div style={{ flex: 1 }}>
+              {logo && <div className="margin-bottom--sm">{logo}</div>}
+              {copyright}
+            </div>
+            <div style={{ flex: 1, display: "flex" }}>
+              {links}
+              <button
+                onClick={() => document.body.scrollIntoView()}
+                style={{
+                  cursor: "pointer",
+                  display: "flex",
+                  alignItems: "center",
+                  fontSize: "1rem",
+                  background: "none",
+                  border: "none",
+                  padding: 0,
+                  margin: 0,
+                  boxShadow: "none",
+                  whiteSpace: "nowrap",
+                  color: "var( --ifm-font-color-base)",
+                }}
               >
-                <path
-                  d="M12 5V19M12 5L16 9M12 5L8 9"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </button>
+                Back to Top{" "}
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M12 5V19M12 5L16 9M12 5L8 9"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </button>
+            </div>
           </div>
         )}
       </div>
